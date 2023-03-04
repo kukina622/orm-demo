7e57cf9259158a6514b78e2c785120ff2e4c7187
Min <wyes91622@gmail.com>
2023-03-05 04:51:30 +0800

    feat: DataModel findAll method

--- src/ORM/DataModel.ts
+++ src/ORM/DataModel.ts
@@ -1,9 +1,29 @@
 import { ORM } from "./ORM";
 
+function createInstance<T>(
+  clazz: new (...args: any[]) => T,
+  ...args: any[]
+): T {
+  return new clazz(...args);
+}
+
 export class DataModel {
   public readonly _field: any | undefined;
-  public _orm: ORM | undefined;
-  public findAll() {}
+  private _orm: ORM | undefined;
+  private _constructor
+
+  constructor(){
+    this._constructor = this.constructor
+  }
+
+  public async findAll<T extends DataModel>(): Promise<Array<T>> {
+    const table = this._constructor.name;
+    const [results] = await this._orm?.query(`SELECT * FROM ${table}`);
+    const resultInstance = results.map((x: any) => {
+      return createInstance(this._constructor as any, x);
+    });    
+    return resultInstance
+  }
   public update() {}
   public save() {}
   public delete() {}
