3f5b5a4bf5a4a92e2a9971ab86a46352f5120373
Min <wyes91622@gmail.com>
2023-03-05 02:57:11 +0800

    feat: implement orm create table method

--- src/ORM/ORM.ts
+++ src/ORM/ORM.ts
@@ -1,10 +1,18 @@
 import mysql from "mysql2";
 import { DataModel } from "./DataModel";
+import { createTableParser } from "./parser";
 
 export class ORM {
   private connection: mysql.Connection;
   constructor(config: mysql.ConnectionOptions) {
     this.connection = mysql.createConnection(config);
   }
-  public register(model: DataModel) {}
+  public register(dataModelRef: typeof DataModel) {
+    const statement = createTableParser(dataModelRef);
+    console.log("[ORM] " + statement);
+
+    this.connection.query(statement, function (err, results, fields) {
+      console.log(results);
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import { ORM, Field, DataType, DataModel } from "./ORM";
+import { ORM, Field, DataType, PK, DataModel } from "./ORM";
 
-class AAA extends DataModel {
-  @Field({ type: DataType.VARCHAR })
-  A: String = "123";
+class Test extends DataModel {
+  @PK
+  @Field({ type: DataType.VARCHAR, length: 20 })
+  name: String | undefined;
+
+  @Field({ type: DataType.INT })
+  age: number | undefined;
 }
+
+const orm = new ORM({
+  database: "test",
+  host: "127.0.0.1",
+  password: "123456",
+  user: "test"
+});
+
+orm.register(Test)
